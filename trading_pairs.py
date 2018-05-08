dc1ecbf2fd9a8f46a9f033ce980628acd77bdc9d
deanturpin <git@lvbtn.uk>
2018-05-08 18:59:28 +0100

    Add trading pairs script

--- trading_pairs.py
+++ trading_pairs.py
@@ -11,29 +11,16 @@ try:
     good_exchanges = {}
 
     # Get all pairs from all exchanges
-    url = 'https://min-api.cryptocompare.com/data/all/exchanges'
+    url = "https://min-api.cryptocompare.com/data/all/exchanges"
     r = requests.get(url)
     exchanges = r.json();
 
-    print("Searching for " + from_symbol + "-" + to_symbol
-            + " over " + str(len(exchanges)) + " exchanges")
-
     for name in exchanges:
-        if from_symbol in exchanges[name]:
-            if to_symbol in exchanges[name][from_symbol]:
-                url = ("https://min-api.cryptocompare.com/data/price?"
-                    + "fsym=" + from_symbol
-                    + "&tsyms=" + to_symbol
-                    + "&e=" + name)
-
-                r = requests.get(url)
-                price = r.json();
-                if price:
-                    good_exchanges[name] = price[to_symbol]
-
-    print(len(good_exchanges), "good exchanges")
-    for name in sorted(good_exchanges, key=lambda x : good_exchanges[x]):
-        print(name, good_exchanges[name])
+        print(name)
+        for from_symbol in exchanges[name]:
+            print("  " + from_symbol)
+            for to_symbol in exchanges[name][from_symbol]:
+                print("    " + to_symbol)
 
 except Exception as e:
     print("exception ", e)
