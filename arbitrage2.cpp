c76acd1abc76decb1b50c104d6f94c8117d09e99
deanturpin <git@lvbtn.uk>
2018-05-09 08:44:22 +0100

    Write pairs into a map

--- arbitrage2.cpp
+++ arbitrage2.cpp
@@ -1,16 +1,36 @@
 #include <string>
+#include <map>
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 int main() {
 
   std::ifstream in("pairs.csv");
 
+  using currency_pairs = std::map < 
+    std::string,
+    std::vector<std::string>
+  >;
+
+  currency_pairs pairs;
+
   std::string line;
-  std::vector<std::string> pairs;
-  while (getline(in, line))
-    pairs.emplace_back(line);
+  while (getline(in, line)) {
+    std::stringstream ss(line);
+    std::string exchange, from, to;
+    ss >> exchange >> from >> to;
+    pairs[from + "-" + to].push_back(exchange);
+  }
 
   std::cout << pairs.size() << " pairs\n";
+
+  for (const auto &p : pairs) {
+    std::cout << p.first;
+    for (const auto &e : p.second)
+      std::cout << ' ' << e;
+
+    std::cout << '\n';
+  }
 }
