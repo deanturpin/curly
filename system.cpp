97c72b0c9716ac235296babf160cd751700ce30f
deanturpin <git@lvbtn.uk>
2018-05-10 20:35:12 +0100

    Add a lambda to wrap creating a CryptoCompare ID

--- system.cpp
+++ system.cpp
@@ -3,9 +3,18 @@
 
 int main() {
 
-  const std::string url =
-    "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR";
+  // Construct a CryptoCompare URL: prepend the domain and append the
+  // application ID
+  const auto cc = [](const std::string &url) {
+    const std::string domain = "https://min-api.cryptocompare.com/";
+    const std::string applic =
+        "&extraParams=https://github.com/deanturpin/arbitrage";
+    return domain + url + applic;
+  };
 
+  // Request some prices and print the response
+  const std::string url = cc("data/price?fsym=BTC&tsyms=USD,JPY,EUR");
   const auto response = curl(url);
+
   std::cout << response << '\n';
 }
