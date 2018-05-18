d66f3f1a18100f3cc151237438a161bfe0986f1f
deanturpin <git@lvbtn.uk>
2018-05-18 08:19:02 +0100

    Set language to cpp

--- bin/all_exchanges.py
+++ bin/all_exchanges.py
@@ -18,20 +18,24 @@ try:
     print("Searching for " + from_symbol + "-" + to_symbol
             + " over " + str(len(exchanges)) + " exchanges")
 
-    for name in exchanges:
-        if from_symbol in exchanges[name]:
-            if to_symbol in exchanges[name][from_symbol]:
-                url = ("https://min-api.cryptocompare.com/data/price?"
-                    + "fsym=" + from_symbol
-                    + "&tsyms=" + to_symbol
-                    + "&e=" + name)
+    # for name in exchanges:
+    #     if from_symbol in exchanges[name]:
+    #         if to_symbol in exchanges[name][from_symbol]:
+    #             url = ("https://min-api.cryptocompare.com/data/price?"
+    #                 + "fsym=" + from_symbol
+    #                 + "&tsyms=" + to_symbol
+    #                 + "&e=" + name)
 
-                r = requests.get(url)
-                price = r.json();
-                if price:
-                    good_exchanges[name] = price[to_symbol]
-                    # print(name, price[to_symbol])
-                    print("\"" + name + "\",")
+    #             r = requests.get(url)
+    #             price = r.json();
+    #             if price:
+    #                 good_exchanges[name] = price[to_symbol]
+    #                 # print(name, price[to_symbol])
+    #                 print("\"" + name + "\",")
+
+    for from_symbol in exchanges["Binance"]:
+        for to_symbol in exchanges["Binance"][from_symbol]:
+            print(from_symbol, "\t", to_symbol)
 
     print(len(good_exchanges), "good exchanges")
     # for name in sorted(good_exchanges, key=lambda x : good_exchanges[x]):
